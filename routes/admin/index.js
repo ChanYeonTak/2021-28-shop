d92188ba2570509faa989965ef883f6511242f2d
ChanYeonTak <rhymeconqueror@gmail.com>
2021-10-27 11:30:39 +0900

    관리자 페이지 작업

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -4,15 +4,13 @@ const express = require('express');
 const router = express.Router();
 
 fs.readdirSync(__dirname)
-  .filter(file => file !== 'index.js')
-  .forEach(file => {
-    const { name, router: childRouter } = require(path.join(__dirname, file))
+  .filter((file) => file !== 'index.js')
+  .forEach((file) => {
+    const { name, router: childRouter } = require(path.join(__dirname, file));
     router.use(name, childRouter);
-    if (name === '/auth') {
-    router.use('/', (req, res, next) => {
-     res.redirect('/admin/auth');
-     })
-    }
-  })
+  });
 
+router.get('/', (req, res, next) => {
+  res.render('admin/index', { css: 'admin-index' });
+});
 module.exports = router;
