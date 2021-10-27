3cdbbd245d1227ff820c94c50e032a1b493fc562
ChanYeonTak <rhymeconqueror@gmail.com>
2021-10-27 12:25:29 +0900

    admin header button type 수정

--- routes/index.js
+++ routes/index.js
@@ -22,14 +22,15 @@
  * [DELETE] shop.soolcloud.com/admin/prd/1 - 상품 삭제
 
   - 게시판 생성자
+ * [GET] shop.soolcloud.com/admin/board/init - 게시판 관리
  * [GET] shop.soolcloud.com/admin/board - 게시판 리스트
  * [GET] shop.soolcloud.com/admin/board/1 - 게시판 내용
  * [PUT] shop.soolcloud.com/admin/board - 게시판 수정
  * [DELETE] shop.soolcloud.com/admin/board/1 - 게시판 삭제
 
-*[GET] shop.soolcloud.com / admin / order - 주문 리스트 *
-  [GET] shop.soolcloud.com / admin / order / 1 - 주문 리스트 *
-  [PUT] shop.soolcloud.com / admin / order - 주문 수정 *
-  [DELETE] shop.soolcloud.com / admin / order / 1 - 주문 삭제
+ *[GET] shop.soolcloud.com/admin/order - 주문 리스트 *
+  [GET] shop.soolcloud.com/admin/order/1 - 주문 리스트 *
+  [PUT] shop.soolcloud.com/admin/order - 주문 수정 *
+  [DELETE] shop.soolcloud.com/admin/order/ 1 - 주문 삭제
 
 **/
