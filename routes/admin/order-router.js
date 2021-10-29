cf8fb3eee115818e7c42f414191d54a0413f2ce5
ChanYeonTak <rhymeconqueror@gmail.com>
2021-10-29 15:38:57 +0900

    카테고리 외 화면 퍼블리싱 완료

--- routes/admin/order-router.js
+++ routes/admin/order-router.js
@@ -1,14 +1,26 @@
 const path = require('path')
 const express = require('express')
 const router = express.Router()
-const { error } = require('../../modules/util')
+const {
+  error
+} = require('../../modules/util')
 
 router.get('/', (req, res, next) => {
   if (req.query.type === 'update') {
-    res.render('admin/order/order-form', { css: 'admin-order' })
+    res.render('admin/order/order-form', {
+      css: 'admin-order'
+    })
   } else {
-    res.render('admin/order/order-list', { css: 'admin-order' })
+    res.render('admin/order/order-list', {
+      css: 'admin-order'
+    })
   }
+})
+
+router.get('/:id', (req, res, next) => {
+  res.render('admin/order/order-form', {
+    css: 'admin-order'
+  })
 })
 
 router.post('/', (req, res, next) => {
@@ -23,4 +35,7 @@ router.delete('/', (req, res, next) => {
   res.send('/admin/order:DELETE')
 })
 
-module.exports = { name: '/order', router }
+module.exports = {
+  name: '/order',
+  router
+}
