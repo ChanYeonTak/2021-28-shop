1cb6111558c47bfed0608b89e98329444de8c3ee
ChanYeonTak <rhymeconqueror@gmail.com>
2021-11-02 10:00:48 +0900

    시드 테스트

--- routes/admin/user-router.js
+++ routes/admin/user-router.js
@@ -12,16 +12,25 @@ const {
 } = require('../../models');
 
 // 회원리스트
-router.get('/', (req, res, next) => {
-  const ejs = {
-    telNumber,
-    type: req.query.type || '',
-  };
-  if (ejs.type === 'create') {
-    res.render('admin/user/user-form', ejs);
-  } else {
-    res.render('admin/user/user-list', ejs);
+router.get('/', async (req, res, next) => {
+  if(req.query.type === 'create') next();
+  else {
+      const ejs = { telNumber };
+      const users = await User.findAll({
+        order: [['id', 'desc']]
+      }) 
+      res.json(users)
+      // res.render('admin/user/user-list', ejs);
   }
+});
+
+// 회원 등록화면
+router.get('/', (req, res, next) => {
+    const ejs = {
+      telNumber,
+      type: req.query.type || '',
+    };
+      res.render('admin/user/user-form', ejs);
 });
 
 // 회원 수정 화면
