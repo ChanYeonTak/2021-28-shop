1fe756a1526bd3b9764bfb9715cae2481954507a
ChanYeonTak <rhymeconqueror@gmail.com>
2021-11-04 10:12:19 +0900

    board inc 완성

--- middlewares/boardinit-mw.js
+++ middlewares/boardinit-mw.js
@@ -1,15 +1,25 @@
+/*
+req.query.boardId -> 현재 게시물에 관련 내용을 findone으로 찾음
+전체 게시판 정보도 가져옴
+*/
+const _ = require('lodash');
 const { BoardInit } = require('../models');
 module.exports = async (req, res, next) => {
-  const binit = await BoardInit.findOne({
-    where: {
-      id: req.query.boardId || 1
-    },
+  let { boardId } = req.query;
+  const boardLists = await BoardInit.findAll({
+    order: [['title', 'desc']],
   });
-  res.locals.boardId = req.query.boardId;
-  res.locals.boardType = binit.boardType;
-  res.locals.boardTitle = binit.title;
-  res.locals.useImg = binit.useImg;
-  res.locals.useFile = binit.useFile;
-  res.locals.useComment = binit.useComment;
+  const [myBoard] = boardLists.filter((v, i) => {
+    if(i === 0 && !boardId) boardId = v.id
+    return v.id == boardId
+  });
+    
+  res.locals.boardLists = _.sortBy(boardLists, 'title');
+  res.locals.boardId = boardId;
+  res.locals.boardType = myBoard.boardType;
+  res.locals.boardTitle = myBoard.title;
+  res.locals.useImg = myBoard.useImg;
+  res.locals.useFile = myBoard.useFile;
+  res.locals.useComment = myBoard.useComment;
   next();
 };
