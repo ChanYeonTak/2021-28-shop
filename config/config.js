03202e1a7e2f4799cb7ce0c193b74778bbd44921
ChanYeonTak <rhymeconqueror@gmail.com>
2021-11-08 15:05:07 +0900

    board-remove 완성

--- config/config.js
+++ config/config.js
@@ -8,6 +8,7 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
+    timezone: '+09:00'
   },
   production: {
     username: process.env.DB_USER,
@@ -16,5 +17,6 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
+    timezone: '+09:00'
   },
 }
