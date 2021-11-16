410aeacc3cd558bc2e2b2046278d77278041c11d
ChanYeonTak <rhymeconqueror@gmail.com>
2021-11-16 11:32:35 +0900

    product update 완료

--- models/Product.js
+++ models/Product.js
@@ -97,25 +97,24 @@ module.exports = (sequelize, { DataTypes, Op }) => {
     data.content = unescape(data.content);
     data.imgs = [];
     data.details = [];
-
-      for (let i = 0; i < 5; i++) {
-        if (data.ProductFiles[i].fieldNum == (i + 1)) {
-          let obj = {
-            thumbSrc: relPath(file.saveName),
-            name: file.oriName,
-            id: file.id,
-            type: file.fileType,
-            fieldNum: file.fieldNum,
-          };
-          if (obj.type === 'F') data.details.push(obj);
-          else data.imgs.push(obj);
-        }
+    if (data.ProductFiles.length) {
+      for (let file of data.ProductFiles) {
+        let obj = {
+          thumbSrc: relPath(file.saveName),
+          name: file.oriName,
+          id: file.id,
+          type: file.fileType,
+          fieldNum: file.fieldNum,
+        };
+        if (obj.type === 'F') data.details.push(obj);
+        else data.imgs.push(obj);
       }
-        delete data.createdAt;
-        delete data.deletedAt;
-        delete data.ProductFiles;
-        return data;
-      };
+    }
+    delete data.createdAt;
+    delete data.deletedAt;
+    delete data.ProductFiles;
+    return data;
+  };
 
   Product.getListData = function (rs, type) {
     const data = rs
@@ -123,9 +122,14 @@ module.exports = (sequelize, { DataTypes, Op }) => {
       .map((v) => {
         v.priceOrigin = numeral(v.priceOrigin).format();
         v.priceSale = numeral(v.priceSale).format();
-        if (v.ProductFiles.fieldNum == '1' && v.ProductFiles.fileType == 'I') {
-          v.img = relPath(file.saveName);
-        } else v.img = 'https://via.placeholder.com/120';
+        let idx = _.findIndex(
+          v.ProductFiles,
+          (v2) => v2.fieldNum == '1' && v2.fileType == 'I'
+        );
+        v.img =
+          idx > -1
+            ? relPath(v.ProductFiles[idx].saveName)
+            : 'https://via.placeholder.com/120';
         delete v.createdAt;
         delete v.deletedAt;
         delete v.ProductFiles;
@@ -158,7 +162,6 @@ module.exports = (sequelize, { DataTypes, Op }) => {
       ],
     });
     const lists = this.getListData(rs);
-
     return { lists, pager, totalRecord: numeral(pager.totalRecord).format() };
   };
 
